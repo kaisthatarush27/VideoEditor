8decbc431ab90911bb566bfff1c6eaa5d575bff7
diatozian <weewoo@diatoz.com>
2023-09-11 17:08:12 +0530

    implmented ui for video effects.

---
+++ app/src/main/java/com/example/videoeditor/EditOperationsPlayerActivity.kt
@@ -0,0 +1,14 @@
+package com.example.videoeditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.videoeditor.databinding.ActivityEditOperationsPlayerBinding
+
+class EditOperationsPlayerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditOperationsPlayerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditOperationsPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
